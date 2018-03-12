0d613561dfaf90d229fb13a0fe1173f66a91a312
sagarambudh <37022238+sagarambudh@users.noreply.github.com>
2018-03-12 22:06:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res){
-    res.send('Article one requested will be deliveres here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+
 });
 app.get('/article-two',function (req,res){
-    res.send('Article two requested will be deliveres here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function (req,res){
-    res.send('Article three requested will be deliveres here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {

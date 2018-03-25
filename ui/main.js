7df1f988cbfdca5732bc1b62708e6afa1a90736e
sagarambudh <37022238+sagarambudh@users.noreply.github.com>
2018-03-25 11:30:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
+//to change the text
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+//to move image
+
+var img = document.getElementById('potter');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
